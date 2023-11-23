672426ed10b6269c8f09069d27c548b0769f1047
joancuni <joancuni2@gmail.com>
2023-11-23 17:34:38 +0100

    added rot_13

--- ft_swap.c
+++ ft_swap.c
@@ -6,9 +6,21 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:21:55 by jocuni-p          #+#    #+#             */
-/*   Updated: 2023/11/16 13:48:16 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:52:14 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*Assignment name  : ft_swap
+Expected files   : ft_swap.c
+Allowed functions: 
+--------------------------------------------------------------------------------
+
+Write a function that swaps the contents of two integers the adresses of which
+are passed as parameters.
+
+Your function must be declared as follows:
+
+void	ft_swap(int *a, int *b);*/
+
 //#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
--- rev_print.c
+++ rev_print.c
@@ -6,12 +6,51 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:51:12 by jocuni-p          #+#    #+#             */
-/*   Updated: 2023/09/18 10:41:43 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2023/11/23 15:40:18 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
 
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "zaz" | cat -e
+zaz$
+$> ./rev_print "dub0 a POIL" | cat -e
+LIOP a 0bud$
+$> ./rev_print | cat -e
+$
+*/
 #include <unistd.h>
 
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    if (ac == 2)
+    {
+        while (av[1][i])
+            i++;
+        i -= 1;
+        while (i >= 0)
+        {
+            write(1, &av[1][i], 1);
+            i--;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
+
+/*------------------OTRA FORMA MENOS CLARA------------------*/
 int main(int ac, char **av)
 {
     int i;
