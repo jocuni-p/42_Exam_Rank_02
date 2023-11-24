3cc90e27a5661587d28ef62181fc639e13475b45
joancuni <joancuni2@gmail.com>
2023-11-24 14:47:59 +0100

    actualizo antes del examen

--- search_and_replace.c
+++ search_and_replace.c
@@ -6,41 +6,60 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 10:34:49 by jocuni-p          #+#    #+#             */
-/*   Updated: 2023/09/11 13:50:03 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2023/11/24 13:32:20 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*Assignment name  : search_and_replace
+  Expected files   : search_and_replace.c
+  Allowed functions: write, exit
+  --------------------------------------------------------------------------------
 
+  Write a program called search_and_replace that takes 3 arguments, the first
+  arguments is a string in which to replace a letter (2nd argument) by
+  another one (3rd argument).
+
+  If the number of arguments is not 3, just display a newline.
+
+  If the second argument is not contained in the first one (the string)
+  then the program simply rewrites the string followed by a newline.
+
+Examples:
+$>./search_and_replace "Papache est un sabre" "a" "o"
+Popoche est un sobre
+$>./search_and_replace "zaz" "art" "zul" | cat -e
+$
+$>./search_and_replace "zaz" "r" "u" | cat -e
+zaz$
+$>./search_and_replace "jacob" "a" "b" "c" "e" | cat -e
+$
+$>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
+ZaZ eT David aiME le METal.$
+$>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
+eNcOre Un ExEmPle Pas Facile a Ecrire $
+*/
 #include<unistd.h>
-#include<stdio.h>
 
-int	ft_strlen(char *str)
+int main(int ac, char **av)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	main(int ac, char **av)
-{
-	int	i;
-
-	i = 0;
-	if (ac == 4 && ft_strlen(av[2]) == 1 && ft_strlen(av[3]) == 1) 
-//	if (av[2][0] < 65 || av[2][0] > 122 || (av[2][0] > 90 && av[2][0] < 97))
+	if (ac == 4)
 	{
-	    while (av[1][i] != '\0')
-	    {
-		    if (av[1][i] == av[2][0])
-			    av[1][i] = av[3][0];
-		    write(1, &av[1][i], 1);
-		    i++;
-	    }
-    }
+		if (((av[2][0] >= 'A' && av[2][0] <= 'Z') || (av[2][0] >= 'a' && av[2][0] <= 'z')) && av[2][1] == '\0')
+		{
+			if (((av[3][0] >= 'A' && av[3][0] <= 'Z') || (av[3][0] >= 'a' && av[3][0] <= 'z')) && av[3][1] == '\0')
+			{
+				while (av[1][i])
+				{
+					if (av[1][i] == av[2][0])
+						av[1][i] = av[3][0];
+					write(1, &av[1][i], 1);
+					i++;
+				}
+			}
+		}
+	}
 	write(1, "\n", 1);
 	return (0);
 }
+
--- ulstr.c
+++ ulstr.c
@@ -6,7 +6,7 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 10:25:34 by jocuni-p          #+#    #+#             */
-/*   Updated: 2023/11/14 16:59:14 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2023/11/24 12:58:25 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*Assignment name  : ulstr
@@ -33,44 +33,24 @@ $>./ulstr | cat -e
 $
 */
 
-
 #include<unistd.h>
-
-void    ft_putchar(char c, int fd)
-{
-    write(fd, &c, 1);
-}
 
 int main(int ac, char **av)
 {
     int i = 0;
 
-	if (ac == 2)
-	{
-    	while (av[1][i] != '\0')
-    	{
-       		if (av[1][i] >= 65 && av[1][i] <= 90)
-            	av[1][i] = av[1][i] + 32;
-        	else if (av[1][i] >= 97 && av[1][i] <= 122)
-            	av[1][i] = av[1][i] - 32;
-			write(1, &av[1][i], 1);
-        	i++;
-    	}
-
-/*-------------------OTRA MANERA VALIDA---------------------*/
-/*
-		while (av[1][i] != '\0')
+    if (ac == 2)
+    {
+        while (av[1][i])
         {
-            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-                ft_putchar(av[1][i] + 32);
-            else if (av[1][i] >= 'a' && av[1][i] <= 'z')
-                ft_putchar(av[1][i] - 32);
-            else
-                write(1, &av[1][i], 1);
+            if (av[1][i] >= 'a' && av[1][i] <= 'z')
+                av[1][i] = av[1][i] - 32;
+            else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+                av[1][i] = av[1][i] + 32;
+            write(1, &av[1][i], 1);
             i++;
         }
-*/
-	}
-    ft_putchar('\n', 1);
-   	return (0);
+    }
+    write(1, "\n", 1);
+    return (0);
 }
